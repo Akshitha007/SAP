8021b295aa5348b4e2e575a43aebe4ba408cbe06
Akshitha007 <113173639+Akshitha007@users.noreply.github.com>
2023-11-20 10:27:02 +0530

    Add files via upload

---
+++ Day3.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"],function(Controller)
+{
+    return Controller.extend ("com.ibm.sampleapp.Day3",{
+        onInit :function() {},
+        onBeforeRendering: function( ) {},
+        onAfterRendering:function(){},
+        onExit: function(){},
+        });
+
+});
